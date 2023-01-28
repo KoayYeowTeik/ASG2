8c1c3ddc6add1368b4deaa852764a309472bd04b
Koay Yeow Teik <S10242544@connect.np.edu.sg>
2023-01-28 11:13:02 +0800

    JS code for the footer (CSS later)

--- header.js
+++ header.js
@@ -11,12 +11,13 @@ header.appendChild(document.createElement("div")).className = "headernav";
 var headernav = document.querySelector(".headernav");
 var headernavimg = headernav.appendChild(document.createElement("img"));
 headernavimg.src = "Images/FACZ-BRICKS-ICON.png";
+headernavimg.addEventListener("click",gohomepage);
 //if login == false:
 var headernavbutton = headernav.appendChild(document.createElement("button"))
 headernavbutton.className =  "headernavbutton";
 headernavbutton.appendChild(document.createTextNode("Sign up / Login"));
 headernavbutton.addEventListener("click",gologinpage);
-headernavbutton.className = "Signup/Login";
+headernavbutton.className = "Login";
 var headernavbutton = headernav.appendChild(document.createElement("button"));
 headernavbutton.appendChild(document.createTextNode("Likes"));
 headernavbutton.addEventListener("click",golikepage);
@@ -33,19 +34,62 @@ headernavbutton.className = "cart";
 var headernotice2 = header.appendChild(document.createElement("div"));
 headernotice2.className = "headernotice2";
 headernotice2.appendChild(document.createTextNode(notice2));
-console.log(header);
+
+
+
+var footer = document.querySelector(".footer");
+var footer1 = footer.appendChild(document.createElement("div"));
+footer1.className = "footer1";
+var p = footer1.appendChild(document.createElement("p"));
+p.appendChild(document.createTextNode("FACZ BRICKS.COM"));
+p.className = "footertitle";
+var p = footer1.appendChild(document.createElement("p"));
+p.appendChild(document.createTextNode("address"));
+p.className = "footeraddress";
+for (i = 0;i<5;i++){
+var span = footer1.appendChild(document.createElement("span"));
+span.className = "fa fa-star checked";}
+footer1.appendChild(document.createElement("br"));
+var a = footer1.appendChild(document.createElement("a"));
+a.href = "reviewpage.html";
+a.appendChild(document.createTextNode("See all reviews"));
+footer1.appendChild(document.createElement("br"));
+var a = footer1.appendChild(document.createElement("a"));
+a.href = "#";
+a.className = "fa fa-instagram";
+var a = footer1.appendChild(document.createElement("a"));
+a.href = "#";
+a.className = "fa fa-youtube";
+var a = footer1.appendChild(document.createElement("a"));
+a.href = "#";
+a.className = "fa fa-facebook";
+footer1.appendChild(document.createElement("br"));
+var footer2 = footer.appendChild(document.createElement("div"));
+footer2.className = "footer2";
+var img = footer2.appendChild(document.createElement("img"));
+img.src = "Images/Paynow.png";
+img.className = "Paynow";
+var img = footer2.appendChild(document.createElement("img"));
+img.src = "Images/Paypal.png";
+img.className = "Paypal";
+var img = footer2.appendChild(document.createElement("img"));
+img.src = "Images/Visa.png";
+img.className = "VIsa";
+var img = footer2.appendChild(document.createElement("img"));
+img.src = "Images/Mastercard.png";
+img.className = "Mastercard";
+var p = footer2.appendChild(document.createElement("p"));
+p.appendChild(document.createTextNode("Warning! Choking Hazard! Some parts may be too small and not suitable for children under age 4!"));
+
+var p = footer.appendChild(document.createElement("p"));
+p.appendChild(document.createTextNode("This website is not legitamate, it is for a school project that uses LEGO as a marketing idea"));
 
 
 
 
-
-
-
-
-
-
-
-
+function gohomepage(event){
+    window.location.href = "homepage.html";
+}
 function gologinpage(event){
     window.location.href = "login.html";
     event.preventDefault();
