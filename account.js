77dac794426aac33d9bc9ffa792d911d80f08f8a
Koay Yeow Teik <S10242544@connect.np.edu.sg>
2023-01-30 12:26:42 +0800

    test commit

--- account.js
+++ account.js
@@ -1,5 +1,18 @@
-var a  = $('button.myprofile');
+var a = document.querySelector(".myprofile");
 a.addEventListener("click",showprofile());
+ 
+
 function showprofile(){
-    $('')
+    $('div.accountcontentmodifier').remove();
+    $('div.accountcontent').append($("<div class = 'accountcontentmodifier'></div>"));
+    $('div.accountcontentmodifier').append($("<div class = 'content1'></div>"));
+    $('div.content1').append($("<p> Coupons</p>"));
+    $('div.content1').append($("<p> You available coupons</p>"));
+    $('div.accountcontentmodifier').append($("<div class = 'content2'></div>"));
+    $('div.accountcontentmodifier').append($("<div class = 'content3'></div>"));
+
 }
+/*function showprofile(){
+    var b = document.querySelector("div.accountcontent");
+    b.removeChild(document.querySelector("div.accountcontentmodifier"));
+}*/
