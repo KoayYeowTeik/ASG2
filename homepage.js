b260f52f652ea3e8715e7dadbf5012ae8aa69418
Koay Yeow Teik <115933415+KoayYeowTeik@users.noreply.github.com>
2023-02-07 19:44:53 +0800

    Update Homepage, for you section auto generateitem

--- homepage.js
+++ homepage.js
@@ -1,4 +1,3 @@
-
 var settings = {
   "async": true,
   "crossDomain": true,
@@ -12,7 +11,14 @@ var settings = {
 }
 let res;
 $.ajax(settings).done(function (response) {
+    var category_list = [];
     for (let i = 0;i<response.length;i++){
+        if (category_list.indexOf(response[i].listing_type.toUpperCase()) == -1){
+            category_list.push(response[i].listing_type.toUpperCase());
+        }
+        else{
+            continue;
+        }
         res = response;
         var ul = document.querySelector("ul.searchresult");
         var li = ul.appendChild(document.createElement("li"));
@@ -22,7 +28,32 @@ $.ajax(settings).done(function (response) {
         a.href = "#";
         a.appendChild(document.createTextNode(response[i].listing_name));
     }
+    for (var i = 0;i<5;i++){
+    var value = Math.floor(Math.random() * response.length);
+    $("div.foryou-listing-container").append($('\
+    <a href = "#">\
+    <div class="listing-container">\
+        <p class="listing-name">'+response[value].listing_name+'</p>\
+        <img class = "listing-image" src="'+response[value].listing_pic+'" alt="">\
+        <p class="listing-price">$'+response[value].listing_price.toFixed(2)+'</p>\
+        <p class="view-click">Click To View</p>\
+    </div>\
+    </a>'))}
 });
+$("div.foryou-listing-container").on("click","a",function(){
+    for(let i = 0;i<res.length;i++){
+        if (res[i].listing_name ==this.children[0].children[0].innerText){
+            sessionStorage.setItem("listing_data",JSON.stringify(res[i]));
+            window.location.href = "listing.html";
+        }
+    }
+})
+
+
+
+
+
+
 function Search(){
     let li = document.getElementsByClassName("result");
     for (let i = 0;i<li.length;i++){
@@ -35,9 +66,9 @@ function Search(){
         else{
             li[i].style.display = "none";
         }
-    }
+    } 
 }
-$(document).on("click", "a", function() {
+$("ul.searchresult").on("click", "a", function() {
     for (let i = 0;i<res.length;i++){
         if (res[i].listing_name == this.innerText){
             sessionStorage.setItem("listing_data",JSON.stringify(res[i]));
@@ -45,6 +76,7 @@ $(document).on("click", "a", function() {
         }
     }
 });
+
 
 
 
